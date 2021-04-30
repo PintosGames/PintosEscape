6fd5e6c00f04f110cb1280902fc92721a3b91827
Filip Mårtensson <fenwikk@protonmail.com>
2021-04-30 11:50:01 +0200

    Added my Discord RPC

---
+++ Assets/Discord RPC/Editor/ERPC.cs
@@ -0,0 +1,263 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using DiscordRPC;
+using DiscordRPC.Logging;
+using DiscordRPC.Unity;
+
+namespace ERPC
+{
+    [InitializeOnLoad]
+    public static class ERPC
+    {
+        public static DiscordRpcClient client;
+
+        public static string projectName;
+        public static string sceneName;
+
+        public static string applicationID = "821629735229980702";
+
+        public static string details;
+        public static string state;
+        public static string largeImageKey = "ab";
+        public static string largeImageText = "Arthur Blue";
+        public static string smallImageKey = "unity";
+        public static string smallImageText = "Made with Unity";
+        public static string firstButtonLabel = "Itch.io";
+        public static string firstButtonUrl = "https://tarsier.itch.io/";
+        public static string secondButtonLabel = "GitHub";
+        public static string secondButtonUrl = "https://github.com/fenwikk/unity-discord-rpc/";
+
+        public static DateTime start;
+
+        public static bool enabled;
+        public static bool enableOnStartup;
+        public static bool resetOnSceneChange = false;
+        public static bool autoUpdate = false;
+
+        public static DiscordPipe targetPipe = DiscordPipe.FirstAvailable;
+
+        public static LogLevel logLevel = LogLevel.Warning;
+
+        public static double lastEdit;
+
+        /// <summary>
+        /// All possible pipes discord can be found on.
+        /// </summary>
+        public enum DiscordPipe
+        {
+            FirstAvailable = -1,
+            Pipe0 = 0,
+            Pipe1 = 1,
+            Pipe2 = 2,
+            Pipe3 = 3,
+            Pipe4 = 4,
+            Pipe5 = 5,
+            Pipe6 = 6,
+            Pipe7 = 7,
+            Pipe8 = 8,
+            Pipe9 = 9
+        }
+
+        static ERPC()
+        {
+            projectName = Application.productName;
+            sceneName = SceneManager.GetActiveScene().name;
+
+            EditorApplication.update += Update;
+
+            start = GetTimeStamp();
+
+            CreateClient();
+            ERPCSettings.GetSettings();
+            if (enableOnStartup) enabled = true;
+            if (enabled) Initialize(); 
+        }
+
+        static void Update()
+        {
+            if (!EditorApplication.isPlaying == false && !client.IsInitialized && enabled) Initialize();
+
+            if (sceneName != SceneManager.GetActiveScene().name)
+            {
+                sceneName = SceneManager.GetActiveScene().name;
+                SceneLoaded();
+            }
+
+            if (enabled) client.Logger.Level = logLevel;
+
+            projectName = Application.productName;
+            sceneName = SceneManager.GetActiveScene().name;
+
+            if (EditorApplication.timeSinceStartup >= lastEdit + 5 && EditorApplication.timeSinceStartup <= lastEdit + 10 && enabled)
+            {
+                UpdateActivity();
+                
+                ERPCWindow.canUpdate = false;
+
+                lastEdit = 0f;
+            }
+
+            if (enabled && client.IsDisposed) Initialize();
+            if (!enabled && !client.IsDisposed) Dispose();
+        }
+
+        static void SceneLoaded(Scene scene = new Scene(), LoadSceneMode mode = new LoadSceneMode())
+        {
+            Debug.Log("Scene Loaded");
+
+            projectName = Application.productName;
+            sceneName = SceneManager.GetActiveScene().name;
+            start = GetTimeStamp();
+            UpdateActivity();
+        }
+
+        public static void CreateClient()
+        {
+            Debug.Log("[ERP] Creating Client");
+
+            //Prepare the logger
+            DiscordRPC.Logging.ILogger logger = null;
+
+            //Update the logger to the unity logger
+            if (Debug.isDebugBuild) logger = new FileLogger("discordrpc.log") { Level = logLevel };
+            if (Application.isEditor) logger = new UnityLogger() { Level = logLevel };
+
+            client = new DiscordRpcClient(
+                applicationID,                                  //The Discord Application ID            
+                pipe: (int)targetPipe,                          //The target pipe to connect too
+                // logger: logger,                              //The logger. Will Uncomment after when Lachee fixes issue #136 in Lachee/discord-rpc-csharp
+                autoEvents: false,                              //WE will manually invoke events
+                client: new UnityNamedPipe()                    //The client for the pipe to use. Unity MUST use a NativeNamedPipeClient since its managed client is broken.
+            );
+        }
+        public static void Initialize()
+        {
+            Debug.Log("[ERP] Creating Client"); 
+
+            //Prepare the logger
+            DiscordRPC.Logging.ILogger logger = null;
+
+            //Update the logger to the unity logger
+            if (Debug.isDebugBuild) logger = new FileLogger("discordrpc.log") { Level = logLevel };
+            if (Application.isEditor) logger = new UnityLogger() { Level = logLevel };
+
+            client = new DiscordRpcClient(
+                applicationID,                                  //The Discord Application ID            
+                pipe: (int)targetPipe,                          //The target pipe to connect too
+                                                                // logger: logger,                              //The logger. Will Uncomment after when Lachee fixes issue #136 in Lachee/discord-rpc-csharp
+                autoEvents: false,                              //WE will manually invoke events
+                client: new UnityNamedPipe()                    //The client for the pipe to use. Unity MUST use a NativeNamedPipeClient since its managed client is broken.
+            );
+
+            Debug.Log("[ERP] Initialize");
+
+            client.Initialize();                                //Connects the client
+
+            ERPCSettings.GetSettings();
+            UpdateActivity();
+        }
+
+        public static void Dispose()
+        {
+            Debug.Log("[ERP] Disposing Client");
+            client.Dispose();
+        }
+
+        public static void UpdateActivity()
+        {
+            if (!ERPCWindow.customDetailsState)
+            {
+                details = projectName;
+                state = sceneName;
+            }
+
+            if (ERPCWindow.secondButtonIsValid)
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = details,
+                    State = state,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = largeImageKey,
+                        LargeImageText = largeImageText,
+                        SmallImageKey = smallImageKey,
+                        SmallImageText = smallImageText
+                    },
+                    Timestamps = new Timestamps
+                    {
+                        Start = start
+                    },
+                    Buttons = new Button[]
+                    {
+                        new Button() { Label = firstButtonLabel, Url = firstButtonUrl},
+                        new Button() { Label = secondButtonLabel, Url = secondButtonUrl}
+                    }
+                });
+            }
+            else if (ERPCWindow.firstButtonIsValid)
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = details,
+                    State = state,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = largeImageKey,
+                        LargeImageText = largeImageText,
+                        SmallImageKey = smallImageKey,
+                        SmallImageText = smallImageText
+                    },
+                    Timestamps = new Timestamps
+                    {
+                        Start = start
+                    },
+                    Buttons = new Button[]
+                    {
+                        new Button() { Label = firstButtonLabel, Url = firstButtonUrl}
+                    }
+                });
+            }
+            else
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = details,
+                    State = state,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = largeImageKey,
+                        LargeImageText = largeImageText,
+                        SmallImageKey = smallImageKey,
+                        SmallImageText = smallImageText
+                    },
+                    Timestamps = new Timestamps
+                    {
+                        Start = start
+                    }
+                });
+            }
+
+            Debug.Log("[ERP] Updated Presence");
+
+            ERPCSettings.SaveSettings();
+
+            client.Logger.Level = logLevel;
+        }
+
+        public static DateTime GetTimeStamp()
+        {
+            if (!resetOnSceneChange)
+            {
+                DateTime timeSinceStartup = DateTime.UtcNow.AddSeconds(-EditorApplication.timeSinceStartup);
+                return timeSinceStartup;
+            }
+            DateTime unixTimestamp = DateTime.UtcNow;
+            return unixTimestamp;
+        }
+    }
+}
+#endif
---
+++ Assets/Discord RPC/Editor/ERPCWindow.cs
@@ -0,0 +1,209 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace ERPC
+{
+    public class ERPCWindow : EditorWindow
+    {
+        private static ERPCWindow _window;
+        public static bool customDetailsState = false;
+
+        public static bool firstButtonIsValid = false;
+        public static bool secondButtonIsValid = false;
+
+        public static bool error = false;
+
+        public static bool canUpdate { get; set; }
+
+        public GUIStyle heading;
+
+        [MenuItem("Window/Editor Rich Presence")]
+        private static void Init()
+        {
+            _window = (ERPCWindow)GetWindow(typeof(ERPCWindow), false, "Editor Rich Presence");
+            _window.Show();
+        }
+
+        public void OnGUI()
+        {
+            #region Toolbar
+
+            EditorGUILayout.BeginHorizontal("Toolbar", GUILayout.ExpandWidth(true));
+
+
+            ERPC.enableOnStartup = GUILayout.Toggle(ERPC.enableOnStartup, "Enable on Startup", "ToolbarButton");
+
+            EditorGUI.BeginChangeCheck();
+
+            GUILayout.FlexibleSpace();
+            
+            ERPC.autoUpdate = GUILayout.Toggle(ERPC.autoUpdate, "Auto Update", "ToolbarButton");
+
+            GUILayout.Space(5);
+
+            EditorGUI.BeginDisabledGroup(!canUpdate);
+            if (GUILayout.Button("Update", "ToolbarButton"))
+            {
+                ERPC.UpdateActivity();
+
+                canUpdate = false;
+
+                ERPC.lastEdit = 0f;
+            }
+            EditorGUI.EndDisabledGroup();
+            EditorGUILayout.EndHorizontal();
+
+            #endregion
+
+            GUILayout.BeginVertical();
+
+            #region Info
+
+            GUILayout.Label("Editor Application ID");
+            ERPC.applicationID = GUILayout.TextField(ERPC.applicationID);
+
+            GUILayout.Space(10);
+
+            #endregion
+
+            #region Details/State
+
+            customDetailsState = EditorGUILayout.ToggleLeft("Custom Details/State", customDetailsState);
+
+            EditorGUI.BeginDisabledGroup(!customDetailsState);
+            GUILayout.Label("Details");
+            ERPC.details = GUILayout.TextField(ERPC.details);
+
+            GUILayout.Label("State");
+            ERPC.state = GUILayout.TextField(ERPC.state);
+            EditorGUI.EndDisabledGroup();
+
+            GUILayout.Space(20);
+
+            #endregion
+
+            ERPC.resetOnSceneChange = EditorGUILayout.ToggleLeft("Reset timestap on scene change", ERPC.resetOnSceneChange);
+
+            #region Images
+
+            GUILayout.Label("Large Image", EditorStyles.boldLabel);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(new GUIContent("Key", "(e.g. Game Icon)"), GUILayout.MaxWidth(30f));
+            ERPC.largeImageKey = GUILayout.TextField(ERPC.largeImageKey, GUILayout.MaxWidth(120f));
+
+            GUILayout.Label(new GUIContent("Text", "(e.g. Game Name)"), GUILayout.MaxWidth(30f));
+            ERPC.largeImageText = GUILayout.TextField(ERPC.largeImageText);
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(10);
+
+            GUILayout.Label("Small Image", EditorStyles.boldLabel);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(new GUIContent("Key", "(e.g. Unity Logo)"), GUILayout.MaxWidth(30f));
+            ERPC.smallImageKey = GUILayout.TextField(ERPC.smallImageKey, GUILayout.MaxWidth(120f));
+
+            GUILayout.Label(new GUIContent("Text", "(e.g. \"Made with Unity\")"), GUILayout.MaxWidth(30f));
+            ERPC.smallImageText = GUILayout.TextField(ERPC.smallImageText);
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(20);
+
+            #endregion
+
+            #region Buttons
+
+            CheckButtons();
+
+            if (firstButtonIsValid) GUILayout.Label("Button 1", EditorStyles.boldLabel);
+            else GUILayout.Label("Button", EditorStyles.boldLabel);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(new GUIContent("Text", "(e.g. \"Itch.io\" or \"Steam\")"), GUILayout.MaxWidth(30f));
+            ERPC.firstButtonLabel = GUILayout.TextField(ERPC.firstButtonLabel, GUILayout.MaxWidth(120f));
+
+            GUILayout.Label(new GUIContent("URL", "(e.g. Itch.io or Steam URL)"), GUILayout.MaxWidth(30f));
+            ERPC.firstButtonUrl = GUILayout.TextField(ERPC.firstButtonUrl);
+            GUILayout.EndHorizontal();
+
+            ButtonErrors(ERPC.firstButtonLabel, ERPC.firstButtonUrl);
+
+            GUILayout.Space(10);
+
+            if (firstButtonIsValid)
+            {
+                GUILayout.Label("Button 2", EditorStyles.boldLabel);
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(new GUIContent("Text", "(e.g. \"GitHub\")"), GUILayout.MaxWidth(30f));
+                ERPC.secondButtonLabel = GUILayout.TextField(ERPC.secondButtonLabel, GUILayout.MaxWidth(120f));
+
+                GUILayout.Label(new GUIContent("URL", "(e.g. GitHub URL)"), GUILayout.MaxWidth(30f));
+                ERPC.secondButtonUrl = GUILayout.TextField(ERPC.secondButtonUrl);
+                GUILayout.EndHorizontal();
+
+                ButtonErrors(ERPC.firstButtonLabel, ERPC.firstButtonUrl);
+            }
+
+            GUILayout.Space(40);
+
+            #endregion
+
+            if (ERPC.enabled)
+            {
+                ERPC.enabled = GUILayout.Toggle(ERPC.enabled, "Shutdown", "Button");
+            }
+            else
+            {
+                ERPC.enabled = GUILayout.Toggle(ERPC.enabled, "Start", "Button");
+            }
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                ERPC.lastEdit = EditorApplication.timeSinceStartup;
+                canUpdate = true;
+            }
+
+            GUILayout.EndVertical();
+        }
+
+        public bool ButtonIsValid(string buttonLabel, string buttonUrl) => ButtonUrlIsValid(buttonUrl) && !string.IsNullOrEmpty(buttonLabel);
+
+        public void CheckButtons()
+        {
+            firstButtonIsValid = ButtonIsValid(ERPC.firstButtonLabel, ERPC.firstButtonUrl);
+            if (firstButtonIsValid)
+            {
+                secondButtonIsValid = ButtonIsValid(ERPC.secondButtonLabel, ERPC.secondButtonUrl);
+            }
+            else
+            {
+                secondButtonIsValid = false;
+            }
+        }
+
+        private void ButtonErrors(string buttonLabel, string buttonUrl)
+        {
+            GUI.contentColor = Color.red;
+
+            if (!ButtonUrlIsValid(buttonUrl) && !string.IsNullOrEmpty(buttonLabel))
+            {
+                GUILayout.Label("URL is not valid!", EditorStyles.boldLabel);
+                canUpdate = false;
+            }
+            else if (string.IsNullOrEmpty(buttonLabel) && !string.IsNullOrEmpty(buttonUrl))
+            {
+                GUILayout.Label("Button text cannot be empty!", EditorStyles.boldLabel);
+                canUpdate = false;
+            }
+
+            GUI.contentColor = Color.white;
+        }
+
+        private bool ButtonUrlIsValid(string url) => Uri.TryCreate(url, UriKind.Absolute, out Uri result)
+            && (result.Scheme == Uri.UriSchemeHttp || result.Scheme == Uri.UriSchemeHttps);
+    }
+}
+#endif
