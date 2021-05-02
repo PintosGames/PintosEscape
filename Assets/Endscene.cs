8bfd9d7890abd411bb0860f7d3d30b4fdd0eed7b
I_like_Unity <83313064+I-like-Unity@users.noreply.github.com>
2021-05-02 20:49:25 +0200

    almost there
    lul

---
+++ Assets/Endscene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endscene : MonoBehaviour
+{
+    public void LoadCredits()
+    {
+        Core.SceneManager.LoadScene(7);
+    }
+}
