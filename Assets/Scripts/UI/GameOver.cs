466a442970b16d704cdcf38d9588c4cc41c27c6e
Filip Mårtensson <fenwikk@protonmail.com>
2021-05-04 20:58:34 +0200

    Few bug fixes

--- Assets/Scripts/UI/GameOver.cs
+++ Assets/Scripts/UI/GameOver.cs
@@ -7,12 +7,14 @@ public class GameOver : MonoBehaviour
     public void Restart()
     {
         Core.SceneManager.ReloadScene();
+
         Core.CoreManager.current.gameOverAnimator.SetBool("open", false);
     }
 
     public void Exit()
     {
         Destroy(FindObjectOfType<AudioManager>());
+        FindObjectOfType<Timer>().timeValue = 0;
 
         Core.SceneManager.LoadScene(0);
         Core.CoreManager.current.gameOverAnimator.SetBool("open", false);
