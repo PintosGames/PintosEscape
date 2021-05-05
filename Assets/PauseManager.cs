e77dc5c9d292d6bc410e1ba5579a7d2711ca9a4e
Filip Mårtensson <fenwikk@protonmail.com>
2021-05-05 12:53:24 +0200

    Added Pause

--- Assets/PauseManager.cs
+++ Assets/PauseManager.cs
@@ -11,56 +11,42 @@ namespace Core
         public static bool isPaused;
         public TextMeshProUGUI timeText;
         public GameObject pauseMenu;
-        public GameObject gameOver;
         public Button pauseButton;
-
-        public Sprite resume;
-        public Sprite pause;
 
         public void Pause()
         {
-            if (!isPaused)
-            {
-                pauseMenu.SetActive(true);
-                timeText.text = Timer.GetTimerTime();
-                isPaused = true;
-                Time.timeScale = 0f;
-            }
-            else
-            {
-                Resume();
-            }
-        }
-
-        public void Resume()
-        {
-            isPaused = false;
-            Time.timeScale = 1f;
-            pauseMenu.SetActive(false);
-        }
-
-        public void BackToMenu()
-        {
-            isPaused = false;
-            FindObjectOfType<Timer>().timeValue = 0;
-            Time.timeScale = 1f;
-            SceneManager.LoadScene(1);
-            pauseMenu.SetActive(false);
+            timeText.text = Timer.GetTimerTime();
+            pauseMenu.SetActive(true);
+            isPaused = true;
+            Time.timeScale = 0f;
         }
 
         public void Restart()
         {
-            isPaused = false;
             FindObjectOfType<Timer>().timeValue = 0;
-            Time.timeScale = 1f;
             SceneManager.ReloadScene();
+        }
+
+        public void Resume()
+        {
             pauseMenu.SetActive(false);
+            isPaused = false;
+            Time.timeScale = 1f;
+        }
+
+        public void BackToMenu()
+        {
+            FindObjectOfType<Timer>().timeValue = 0;
+            pauseMenu.SetActive(false);
+            isPaused = false;
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(0);
         }
 
         private void Update()
         {
-            if (!pauseMenu.activeInHierarchy || !gameOver.activeInHierarchy) pauseButton.image.sprite = pause;
-            else pauseButton.image.sprite = resume;
+            if (pauseMenu.activeInHierarchy || !CoreManager.current.scene.scenes[CoreManager.current.scene.currentBuildIndex].gameScene) pauseButton.gameObject.SetActive(false);
+            else pauseButton.gameObject.SetActive(true);
         }
     }
 
